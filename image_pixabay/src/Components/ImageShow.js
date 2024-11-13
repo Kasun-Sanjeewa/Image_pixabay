b999fe649384b85860e70e57efea63ad5ea9c511
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-11-13 19:17:24 +0530

    ok

--- image_pixabay/src/Components/ImageShow.js
+++ image_pixabay/src/Components/ImageShow.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ImageShow({ imageShowProp }) {
     return (
         <div >
-            <img src={imageShowProp.webformatURL} alt={imageShowProp.tags} ></img>
+            <a href={imageShowProp.largeImageURL}><img src={imageShowProp.webformatURL} alt={imageShowProp.tags} ></img></a>
+
         </div>
     )
 }
